20e1f423d5600cc08aef2331376fbd01a69c4a10
tomjordancc <tomjordan505@gmail.com>
2018-09-06 15:43:54 +1000

    few things on prac 5

--- Prac_5/words.py
+++ Prac_5/words.py
@@ -2,18 +2,21 @@
 CP1404/CP5632 Practical - Suggested Solution
 Count word occurrences in a string
 """
+def main()
+    DIFFERENT_WORDS = {}
+    user_input = input("Text: ")
 
-DIFFERENT_WORDS = {}
-user_input = input("Text: ")
+    words = user_input.split()
+    for word in words:
+        occurances = DIFFERENT_WORDS.get(word, 0)
+        DIFFERENT_WORDS[word] = occurances + 1
 
-words = user_input.split()
-for word in words:
-    occurances = DIFFERENT_WORDS.get(word, 0)
-    DIFFERENT_WORDS[word] = occurances + 1
+    words = list(DIFFERENT_WORDS.keys())
+    words.sort()
 
-words = list(DIFFERENT_WORDS.keys())
-words.sort()
+    length_words = max((len(word) for word in words))
+    for word in words:
+        print("{} : {}".format(word.ljust(length_words),  DIFFERENT_WORDS[word]))
 
-length_words = max((len(word) for word in words))
-for word in words:
-    print("{} : {}".format(word.ljust(length_words),  DIFFERENT_WORDS[word]))
+
+main()
