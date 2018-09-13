4b91162782e188cda41a3d1b6322f08c4566f711
Tom <tmojordan505@gmail.com>
2018-09-13 14:14:34 +1000

    getting first part

--- Prac_6/used cars.py
+++ Prac_6/used cars.py
@@ -6,7 +6,7 @@ from Prac_6.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car("car", 180)
     my_car.drive(30)
     print("fuel =", my_car.fuel)
     print("odo =", my_car.odometer)
@@ -15,5 +15,10 @@ def main():
     print("Car {}, {}".format(my_car.fuel, my_car.odometer))
     print("Car {self.fuel}, {self.odometer}".format(self=my_car))
 
+    limo = Car("Limo", 100)
+    print(limo)
+    limo.add_fuel(20)
+    limo.drive(115)
+    print(limo.odometer)
 
 main()
