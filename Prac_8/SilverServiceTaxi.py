82d04e3c0cf807e675a9a5a9bb86877862877eaa
tomjordan505@gmail.com <Yakity123>
2018-10-04 15:24:49 +1000

    fixed some things

--- Prac_8/SilverServiceTaxi.py
+++ Prac_8/SilverServiceTaxi.py
@@ -3,7 +3,7 @@ from Prac_8.taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
     """Specialised version of a Car that includes fare costs."""
-    #flagfall = float(4.5)
+    flagfall = 4.5
 
     def __init__(self, name, fuel, fanciness):
         """Initialise a Taxi instance, based on parent class Car."""
@@ -13,11 +13,11 @@ class SilverServiceTaxi(Taxi):
 
     def __str__(self):
         """Return a string like a Car but with current fare distance."""
-        return "{} plus flagfall cost of {:.2f}/km".format(super().__str__(),
-                                                          4.5)
         #return "{} plus flagfall cost of {:.2f}/km".format(super().__str__(),
-         #                                                  flagfall)
+         #                                                 4.5)
+        return "{} plus flagfall cost of {:.2f}/km".format(super().__str__(),
+                                                           self.flagfall)
 
     def get_fare(self):
         """Return the price for the taxi trip."""
-        return self.get_fare() + 4.5
+        return self.get_fare() + self.flagfall
